b8d17db4494e6d565409c8883072c69994857040
mfutech <mfutech@users.noreply.github.com>
2025-10-26 18:21:38 +0100

    move home to hostlist using route alias

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -6,12 +6,13 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/certificates',
       name: 'home',
       component: () => import('../components/CertificatesList.vue'),
     },
     {
       path: '/hosts',
+      alias: '/',
       name: 'hostslist',
       component: () => import('../components/HostsList.vue'),
     },
