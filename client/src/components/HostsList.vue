0365188a779988c43a3818733291abdc448b4a76
mfutech <mfutech@users.noreply.github.com>
2025-10-29 00:10:08 +0100

    factorize GompHostsList

--- client/src/components/HostsList.vue
+++ client/src/components/HostsList.vue
@@ -1,134 +1,20 @@
 <script>
 import axios from 'axios';
 import * as bootstrap from 'bootstrap';
-import DataTable from 'datatables.net-vue3'
-import DataTablesCore from 'datatables.net-bs5';
-import 'datatables.net-responsive';
-import 'datatables.net-select';
 
+import CompHostsList from '../components/CompHostsList.vue';
 
-DataTablesCore.use(bootstrap);
-DataTable.use(DataTablesCore);
-
-console.log('DataTablesCore:', DataTablesCore);
 
 export default {
   name: 'HostsList',
   components: {
-    DataTable,
+    CompHostsList,
   },
+
   data() {
     return {
       hosts: [],
-      options: {
-        responsive: true,
-        select: true,
-        length: 500,
-        lengthMenu: [10, 25, 50, { label: 'All', value: -1 }]
-      },
-      columns: [
-        { title: 'Date', data: 'last_scan' },
-        {
-          title: 'Host', data: 'host',
-          render: function (data, type, row, meta) {
-            if (type === 'display') {
-              data = '<a href="/host/' + data + '/' + row.port + '" class="btn btn-primary">' + data + ':' + row.port + '</a>';
-            }
-            // console.log('Rendering host column:', data, type, row, meta);
-            return data;
-          }
-        },
-        // {
-        //   title: 'Host', data: 'host', render: function (data, type, row, meta) {
-        //     if (type === 'display') {
-        //       data = data + ':' + row.port;
-        //     }
-        //     return data;
-        //   }
-        // },
-        {
-          title: 'SSLv2', data: 'sslv2', render: function (data, type, row, meta) {
-            let result = data
-              result = '<span>' + (data.enabled ? 'SSLv2' : '-') + '</span>';
-            return result;
-          }
-        },
-        {
-          title: 'SSLv3', data: 'sslv3', render: function (data, type, row, meta) {
-            let result = data
-              result = '<span>' + (data.enabled ? 'SSLv3' : '-') + '</span>';
-            return result;
-          }
-        },
-        {
-          title: 'TLS 1.0', data: 'tls1_0', render: function (data, type, row, meta) {
-            let result = data
-              result = '<span>' + (data.enabled ? 'TLS1.0' : '-') + '</span>';
-            return result;
-          }
-        },
-        {
-          title: 'TLS 1.1', data: 'tls1_1', render: function (data, type, row, meta) {
-            let result = data
-              result = '<span>' + (data.enabled ? 'TLS1.1' : '-') + '</span>';
-            return result;
-          }
-        },
-        {
-          title: 'TLS 1.2', data: 'tls1_2', render: function (data, type, row, meta) {
-            let result = data
-              result = '<span>' + (data.enabled ? 'TLS1.2' : '-') + '</span>';
-            return result;
-          }
-        },
-        {
-          title: 'TLS 1.3', data: 'tls1_3', render: function (data, type, row, meta) {
-            let result = data
-              result = '<span>' + (data.enabled ? 'TLS1.3' : '-') + '</span>';
-            return result;
-          }
-        },
-        {
-          title: 'Old', data: 'moz_old', render: function (data, type, row, meta) {
-            let result = data
-            if (type === 'display') {
-              result = '<span>' + (data.compliant ? 'Yes' : 'No') + '</span>';
-            }
-            return result;
-          }
-        },
-        {
-          title: 'Intermediate', data: 'moz_intermediate', render: function (data, type, row, meta) {
-            let result = data
-            if (type === 'display') {
-              result = '<span>' + (data.compliant ? 'Yes' : 'No') + '</span>';
-            }
-            return result;
-          }
-        },
-        {
-          title: 'Modern', data: 'moz_modern', render: function (data, type, row, meta) {
-            let result = data
-            if (type === 'display') {
-              result = '<span>' + (data.compliant ? 'Yes' : 'No') + '</span>';
-            }
-            return result;
-          }
-        },
-        {
-          title: 'Serial Number', data: 'certificate_serial_number', "render": function (data, type, row, meta) {
-            if (type === 'display') {
-              let output = '';
-              data.forEach(function (serial) {
-                let serial_disp = serial.length > 18 ? serial.substr(0, 15) + '...' : serial;
-                output += '<a class="btn btn-primary" href="/certificate/' + serial + '">' + serial_disp + '</a><br>';
-              });
-              data = output;
-            }
-            return data;
-          }
-        },
-      ],
+
     };
   },
   methods: {
@@ -171,7 +57,7 @@ export default {
         <a href="/Ping" type="button" class="btn btn-primary btn-sm">Ping</a>
         <br><br>
 
-        <DataTable :columns="columns" :options="options" :data="hosts" class="table table-hover display nowrap" />
+        <CompHostsList :hosts="hosts" />
 
       </div>
     </div>
